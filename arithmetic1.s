7c61d0de94899f75fd5fac47d28565b71cf430b5
Mustafa Ali <mali39@student.gsu.edu>
2020-02-04 20:23:17 -0500

    added arithmetic code for mustafa

---
+++ arithmetic1.s
@@ -0,0 +1,16 @@
+@ second program
+.section .data
+.section .text
+.global _start
+_start:
+	mov r1, #10
+	mov r2, #11
+	add r3, r1, r2 
+	mov r4, #7
+	mov r5, #2
+	mul r8, r4, r5
+	sub r9, r3, r8
+
+	mov r7, #1
+	svc #0
+.end
