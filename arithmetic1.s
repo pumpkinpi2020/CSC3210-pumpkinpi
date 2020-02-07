c7165578f8c19ba34fff59f9a972dc42621a850c
rominaka93 <60592979+rominaka93@users.noreply.github.com>
2020-02-07 13:02:08 -0500

    Add files via upload

--- arithmetic1.s
+++ arithmetic1.s
@@ -1,16 +1,23 @@
-@ second program
-.section .data
-.section .text
-.global _start
-_start:
-	mov r1, #10
-	mov r2, #11
-	add r3, r1, r2 
-	mov r4, #7
-	mov r5, #2
-	mul r8, r4, r5
-	sub r9, r3, r8
 
-	mov r7, #1
-	svc #0
+
+
+@ second program
+ .section .data
+ .section .text
+ .global _start
+_start:
+   mov r1, #10  @load r1 with 10
+   mov r2, #11  @load r2 with 11
+   mov r3, #7   @load r3 with 7
+   mov r4, #2   @load r4 with 2
+
+  add r5, r1, r2   @add  r1 and r2 , store in r5
+  mul r6, r3, r4   @multiply of r3 and r4, store in r6
+  sub r1, r5, r6   @substract r5 and r6, store in r1
+
+ mov r7,#1
+  svc #0
+
 .end
+
+
