6bc1d4bef24fa3cd511da7ca4250f788c8142194
mali0039 <60575702+mali0039@users.noreply.github.com>
2020-02-08 16:02:06 -0500

    Mustafa's code

--- arithmetic1.s
+++ arithmetic1.s
@@ -1,23 +1,16 @@
-
-
-
 @ second program
- .section .data
- .section .text
- .global _start
+.section .data
+.section .text
+.global _start
 _start:
-   mov r1, #10  @load r1 with 10
-   mov r2, #11  @load r2 with 11
-   mov r3, #7   @load r3 with 7
-   mov r4, #2   @load r4 with 2
+	mov r1, #10
+	mov r2, #11
+	add r3, r1, r2 
+	mov r4, #7
+	mov r5, #2
+	mul r8, r4, r5
+	sub r1, r3, r8
 
-  add r5, r1, r2   @add  r1 and r2 , store in r5
-  mul r6, r3, r4   @multiply of r3 and r4, store in r6
-  sub r1, r5, r6   @substract r5 and r6, store in r1
-
- mov r7,#1
-  svc #0
-
+	mov r7, #1
+	svc #0
 .end
-
-
